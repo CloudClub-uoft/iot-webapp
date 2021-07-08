f86f15c4fe81fe8b298b3ae55a996aef53787634
Ian <IanWebster2828@gmail.com>
2021-07-07 22:40:36 -0400

    Register page progress. Fetch function works

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from './components/layout/sidebar';
 import Navbar from './components/layout/navbar';
 import CardGrid from './components/layout/cards/CardGrid';
+import Register from './components/layout/Register';
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <div className="App">      
       <Navbar/>
       <br></br>
-      <CardGrid />     
+      {/* <CardGrid />  */}
+      <Register />    
       {/* Currently the sidebar is causing overlay issues. Uncomment the next line to see its behaviour. Reference: https://material-ui.com/components/drawers/ */}
       {/* <Sidebar/> */}      
     </div>    
--- src/components/layout/Register.js
+++ src/components/layout/Register.js
@@ -1,23 +1,99 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { useState, useEffect } from 'react';
+import Icon from '@material-ui/core/Icon';
+import SendIcon from '@material-ui/icons/Send';
 
-
-// Also setup router to get to this page
-
-// also define routes for router?
-
-/*
-IoT Device Registration Page
-https://jasonwatmore.com/post/2020/02/01/react-fetch-http-post-request-examples
+/* TODO: 
+- Revamp fetch
+- Center text inputs
+- Error handling (correct input format for mac, name)
 */
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+    button: {
+        margin: theme.spacing(1),
+    }
+}));
+
+// Device registration
 export default function Register() {
-    const register_url = 'http://localhost:3000/device/register';
+    const classes = useStyles();
+    return (
+        <form className={classes.root} noValidate autoComplete="off">
+            {/* <TextField id="standard-basic" label={data['apiKey']} /> */}
+            <TextField
+                id="api-key-text-field"
+                label="Read Only"
+                defaultValue="API Key"
+                InputProps={{
+                    readOnly: true,
+                }}
+                variant="outlined"
+            />
+            {/* <TextField align='center' id="filled-basic" label="Filled" variant="filled" />
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+            <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+                endIcon={<SendIcon />}
+                onClick={() => fetch_api_key()}
+            >
+                Get API Key
+            </Button>
+        </form>
+    );
+    
+}
+
+// Use /device/register route to register device with MAC address and Name provided
+const fetch_api_key = () => {
+    const server_ip = '10.0.0.32'; 
+    const register_url = `http://${server_ip}:3000/device/register`;
     const options = {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 
+            'Content-Type': 'application/json',
+        },
         body: JSON.stringify({ 
-            "deviceId": "aa:aa:aa:aa:aa", 
+            "deviceId": "4D:01:F8:1F:80:5F", // TODO: take in these parameters from text fields.
             "friendlyName": "Ian Test"
         })
     };
 
-    // fetch();
+    let fetched;
+
+    // TODO: handle errors correctly
+    postData(register_url, options)
+    .then(data => {
+        console.log(data);
+        fetched = data;
+    })
+    .then(() => {
+        // console.log(fetched);
+        // let api_key_text_field = document.getElementById('api-key-text-field');
+        // TODO: set text box to API key
+
+
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+    return;
+};
+
+// Posts data asynchronously, returns response
+async function postData(url, options) {
+    const response = await fetch(url, options);
+    return response.json();
 }
