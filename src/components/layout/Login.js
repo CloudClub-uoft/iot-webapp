650014e564afb393f266e5ff362dc1ed747051fe
Adam S <72041542+AdamS260@users.noreply.github.com>
2021-08-17 14:41:02 -0400

    Created login page

--- src/components/layout/Login.js
+++ src/components/layout/Login.js
@@ -5,10 +5,18 @@ import { Typography, Button, Link } from "@material-ui/core";
 import Container from '@material-ui/core/Container';
 import Avatar from '@material-ui/core/Avatar';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from '@material-ui/core/Input';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { InputAdornment } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import VisibilityIcon from '@material-ui/icons/VisibilityOff';
+import { OutlinedInput } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   form: {
-    maxWidth: 200,
+    maxWidth: 220,
     alignItems: 'center',
     textAlign: 'center',
     marginTop: theme.spacing(1),
@@ -35,26 +43,25 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const LoginForm = () => {
+const LoginForm = (props) => {
+  const [showPassword, setShow] = React.useState(false);
   const classes = useStyles();
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
+  const handleMouseDownPassword = (e) => {
+    e.preventDefault();
+  }
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
+  const handleClickShowPassword = (e) => {
+    setShow(!showPassword);
+  }
 
   return (
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <AccountCircleIcon/>
+          <LockOutlinedIcon />
         </Avatar>
-        <form className={classes.form}>
+        <form className={classes.form} >
           <Typography variant="h4">Login</Typography>
           <div className={classes.inputInfo}>
             <Typography variant="subtitle2">
@@ -66,21 +73,34 @@ const LoginForm = () => {
               id="Email"
               name="Email"
               autoComplete="email"
-              autoFocus
               margin='normal'
-              onChange={handleEmailChange}
+              onChange={props.onEmailChange}
+              maxwidth
+              InputProps={{style: { padding:"0px 1.5ch 0px 1.5ch"},}}
             />
             <TextField
               label="Password"
               variant="outlined"
               id="Password"
               autoComplete="current-password"
-              autoFocus
               margin='normal'
-              onChange={handlePasswordChange}
-            />
+              onChange={props.onPasswordChange}
+              showPassword={showPassword}
+              maxwidth
+              InputProps={{
+                style: { padding:"0px 0px 0px 1.5ch"},
+                endAdornment: (
+                  <InputAdornment position="start">
+                    <IconButton onClick={handleClickShowPassword} onMouseDown={handleMouseDownPassword}>
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+                type: (showPassword ? "text" : "password"),
+              }} /> 
+
           </div>
-          <Button type="submit" variant="contained" className={classes.button}>
+          <Button type="submit" variant="contained" className={classes.button} onClick={props.onClick}>
             Login
           </Button>
         </form>
