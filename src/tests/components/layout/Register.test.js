a98bde154e0652bd00099582bf3532cf74eeae4a
iWebster28 <iWebster28@users.noreply.github.com>
2021-09-22 20:02:32 -0400

    Finish register tests.

--- src/tests/components/layout/Register.test.js
+++ src/tests/components/layout/Register.test.js
@@ -6,6 +6,7 @@ import Register from '../../../components/layout/Register';
 import {setupServer} from 'msw/node';
 import { rest } from 'msw';
 
+// Setup Mock Server
 const server = setupServer(
   rest.post('http://localhost:8080/api/register', (req, res, ctx) => {
     return res(ctx.json({Status: 'Success'}));
@@ -16,6 +17,7 @@ beforeAll(() => server.listen());
 afterEach(() => server.restoreHandlers());
 afterAll(() => server.close());
 
+// Register Tests
 describe('Register Page', () => {
   describe('1. Functional Tests', () => {
     it('1a. Enter a friendly name.', () => {
@@ -44,7 +46,7 @@ describe('Register Page', () => {
   });
 
   describe('2. Negative Tests', () => {
-    it('2a. No friendly name entered.', async () => {
+    it('2a. Bad Request.', async () => {
       const expected = "Bad Request";
       // Mimic bad request
       server.use(
@@ -53,15 +55,23 @@ describe('Register Page', () => {
         }),
       );
       const utils = render(<Register />);
-      const friendly = utils.getByRole('textbox', {name: "Friendly Name" /*label*/}); 
-      fireEvent.change(friendly, {target: {value: 'Register Test Friendly Name'}});
       // Click register
       fireEvent.click(screen.getByRole('button'));
       await waitFor(() => screen.getByText(expected));
-      // await screen.findByText('Device Successfully Registered!');
-      // expect(screen.getByLabelText('toast').innerHTML).toBe("Device Successfully Registered!");
+      // await screen.findByText('Device Successfully Registered!'); // alternative
+      // expect(screen.getByLabelText('toast').innerHTML).toBe("Device Successfully Registered!"); //alternative
     });
+
+    // These tests have no implemented frontend features currently (i.e. tooltips for incorrect inputs)
+
     // it('2b. No device id entered.', () => {
+    //   const expected = "Bad Request";
+    //   // Mimic bad request
+    //   server.use(
+    //     rest.post('http://localhost:8080/api/register', (req, res, ctx) => {
+    //       return res(ctx.json({error: expected}));
+    //     }),
+    //   );
     //   const utils = render(<Register />);
     //   const actual = utils.getByRole('textbox', {name: "Device ID" /*label*/}); 
     //   fireEvent.change(actual, {target: {value: ''}});
@@ -70,6 +80,12 @@ describe('Register Page', () => {
 
     // });
     // it('2c. Invalid device id entered.', () => {
+    //   // Mimic bad request
+    //   server.use(
+    //     rest.post('http://localhost:8080/api/register', (req, res, ctx) => {
+    //       return res(ctx.json({error: expected}));
+    //     }),
+    //   );
     //   const utils = render(<Register />);
     //   const actual = utils.getByRole('textbox', {name: "Device ID" /*label*/}); 
     //   fireEvent.change(actual, {target: {value: '12:34:56:AB:CD:EF'}});
